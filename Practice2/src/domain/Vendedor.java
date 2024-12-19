63e9d054724b5c9c8fd6b82424349fa3fcf1e380
JBDolmus <jbautista.dormo.corea@gmail.com>
2024-12-19 13:54:41 -0600

    Completed of action Create, Upadte, Read and Delete

--- Practice2/src/domain/Vendedor.java
+++ Practice2/src/domain/Vendedor.java
@@ -4,6 +4,7 @@ package domain;
 
 public class Vendedor extends Persona {
     private String codVendedor;
+    private String dateBirth;
 
     public String getCodVendedor() {
         return codVendedor;
@@ -12,5 +13,13 @@ public class Vendedor extends Persona {
     public void setCodVendedor(String codVendedor) {
         this.codVendedor = codVendedor;
     }
+
+	public String getDateBirth() {
+		return dateBirth;
+	}
+
+	public void setDateBirth(String dateBirth) {
+		this.dateBirth = dateBirth;
+	}
     
 }
--- Practice2/src/module-info.java
+++ Practice2/src/module-info.java
@@ -1,9 +1,11 @@
 module Practice2 {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	
 	opens controller;
 	exports controller;
+	exports domain;
 }
